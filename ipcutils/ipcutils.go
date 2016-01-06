5f0782864b42dd625171ba3f1236f99f8f7b2be6
Avoy Nanda <ananda@snaproute.com>
2016-01-05 16:17:21 -0800

    Socket close notification

---
+++ ipcutils/ipcutils.go
@@ -0,0 +1,31 @@
+package ipcutils
+
+import (
+	"fmt"
+	"git.apache.org/thrift.git/lib/go/thrift"
+)
+
+func SocketCloseNotification(clnt interface{}) (err error) {
+	fmt.Println("### Socket closed for client ", clnt)
+	return nil
+}
+
+//
+// This method gets Thrift related IPC handles.
+//
+func CreateIPCHandles(address string, clnt interface{}) (thrift.TTransport, *thrift.TBinaryProtocolFactory) {
+	var transportFactory thrift.TTransportFactory
+	var transport thrift.TTransport
+	var protocolFactory *thrift.TBinaryProtocolFactory
+	var err error
+
+	protocolFactory = thrift.NewTBinaryProtocolFactoryDefault()
+	transportFactory = thrift.NewTTransportFactory()
+	transport, err = thrift.NewTSocketTimeout(address, 2*60*1000, SocketCloseNotification, clnt)
+	transport = transportFactory.GetTransport(transport)
+	if err = transport.Open(); err != nil {
+		//logger.Println("Failed to Open Transport", transport, protocolFactory)
+		return nil, nil
+	}
+	return transport, protocolFactory
+}
