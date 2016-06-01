4886e13ee323aba2925ab8522d5ea8724a75bd57
Hari Gollapalli <hgollapalli@snaproute.com>
2016-05-31 17:28:18 -0700

    Added base daemon

---
+++ dmnBase/fsdmn.go
@@ -0,0 +1,57 @@
+//
+//Copyright [2016] [SnapRoute Inc]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//       Unless required by applicable law or agreed to in writing, software
+//       distributed under the License is distributed on an "AS IS" BASIS,
+//       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//       See the License for the specific language governing permissions and
+//       limitations under the License.
+//
+// _______  __       __________   ___      _______.____    __    ____  __  .___________.  ______  __    __
+// |   ____||  |     |   ____\  \ /  /     /       |\   \  /  \  /   / |  | |           | /      ||  |  |  |
+// |  |__   |  |     |  |__   \  V  /     |   (----` \   \/    \/   /  |  | `---|  |----`|  ,----'|  |__|  |
+// |   __|  |  |     |   __|   >   <       \   \      \            /   |  |     |  |     |  |     |   __   |
+// |  |     |  `----.|  |____ /  .  \  .----)   |      \    /\    /    |  |     |  |     |  `----.|  |  |  |
+// |__|     |_______||_______/__/ \__\ |_______/        \__/  \__/     |__|     |__|      \______||__|  |__|
+//
+
+package dmnBase
+
+import (
+	"fmt"
+	"utils/logging"
+)
+
+type FSDaemon struct {
+	DmnName   string
+	LogPrefix string
+}
+
+func (dmn *FSDaemon) Init(dmnName string, logPrefix string) bool {
+	dmn.DmnName = dmnName
+	dmn.LogPrefix = logPrefix
+	fmt.Printf("%s Starting %s logger", dmn.LogPrefix, dmn.DmnName)
+	logger, err := logging.NewLogger(dmnName, logPrefix, true)
+	if err != nil {
+		fmt.Println("Failed to start the logger. Nothing will be logged...")
+	}
+	logger.Info(fmt.Sprintln("Starting %s server...", dmnName))
+	return true
+}
+
+func (dmn *FSDaemon) GetParams() string {
+
+	paramsDir := flag.String("params", "./params", "Params directory")
+	flag.Parse()
+	fileName := *paramsDir
+	if fileName[len(fileName)-1] != '/' {
+		fileName = fileName + "/"
+	}
+	return fileName
+}
