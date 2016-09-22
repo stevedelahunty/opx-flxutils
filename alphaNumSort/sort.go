31da5f87d40f9c537c9fe33e0396e12d6fa24ec3
Avoy Nanda <ananda@snaproute.com>
2016-09-22 11:41:36 -0700

    Compare function in alphaNumSort

--- alphaNumSort/sort.go
+++ alphaNumSort/sort.go
@@ -59,6 +59,18 @@ func Sort(strList []string) []string {
 	return outList
 }
 
+func Compare(s1, s2 string) int {
+	wt1 := computeWeight(s1)
+	wt2 := computeWeight(s2)
+	if wt1 < wt2 {
+		return -1
+	} else if wt1 == wt2 {
+		return 0
+	} else {
+		return 1
+	}
+}
+
 /* Computes weight of give string. Max char ascii val ('z') = 122 */
 func computeWeight(str string) float64 {
 	var wt float64
