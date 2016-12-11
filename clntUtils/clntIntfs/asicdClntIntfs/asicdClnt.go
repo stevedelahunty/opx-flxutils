9a85d5e456f96ba0ecf79b09a0ad5e29c6acaafa
Ashutosh Shanker <ashanker@snaproute.com>
2016-12-11 11:27:51 -0800

    Incorporatin review comments to move daemon clnt to seperate packages

--- clntUtils/clntIntfs/asicdClntIntfs/asicdClnt.go
+++ clntUtils/clntIntfs/asicdClntIntfs/asicdClnt.go
@@ -21,39 +21,50 @@
 // |__|     |_______||_______/__/ \__\ |_______/        \__/  \__/     |__|     |__|      \______||__|  |__|
 //
 
-package clntIntfs
+package asicdClntIntfs
 
 import (
 	"asicd/fsAsicdClnt"
 	"errors"
-	"utils/clntUtils/clntDefs"
+	"utils/clntUtils/clntDefs/asicdClntDefs"
+)
+
+type AsicdClntPluginName string
+
+const (
+	FS_ASICD_CLNT AsicdClntPluginName = "FSAsicdClnt"
 )
 
 type AsicdClntIntf interface {
 	CreateIPv4Neighbor(ipAddr string, macAddr string, vlanId int32, ifIdx int32) (int32, error)
 	UpdateIPv4Neighbor(ipAddr string, macAddr string, vlanId int32, ifIdx int32) (int32, error)
 	DeleteIPv4Neighbor(ipAddr string) (int32, error)
-	GetBulkIPv4IntfState(curMark, count int) (*clntDefs.IPv4IntfStateGetInfo, error)
+	GetBulkIPv4IntfState(curMark, count int) (*asicdClntDefs.IPv4IntfStateGetInfo, error)
 
 	CreateIPv6Neighbor(ipAddr string, macAddr string, vlanId int32, ifIdx int32) (int32, error)
 	UpdateIPv6Neighbor(ipAddr string, macAddr string, vlanId int32, ifIdx int32) (int32, error)
 	DeleteIPv6Neighbor(ipAddr string) (int32, error)
 
-	GetBulkPort(curMark, count int) (*clntDefs.PortGetInfo, error)
-	GetBulkPortState(curMark, count int) (*clntDefs.PortStateGetInfo, error)
-	GetBulkVlan(curMark, count int) (*clntDefs.VlanGetInfo, error)
-	GetBulkVlanState(curMark, count int) (*clntDefs.VlanStateGetInfo, error)
+	GetBulkPort(curMark, count int) (*asicdClntDefs.PortGetInfo, error)
+	GetBulkPortState(curMark, count int) (*asicdClntDefs.PortStateGetInfo, error)
+	GetBulkVlan(curMark, count int) (*asicdClntDefs.VlanGetInfo, error)
+	GetBulkVlanState(curMark, count int) (*asicdClntDefs.VlanStateGetInfo, error)
 
 	CreateLag(ifname string, hashType int32, ports string) (int32, error)
 	DeleteLag(ifIndex int32) (int32, error)
 	UpdateLag(ifIndex, hashType int32, ports string) (int32, error)
 	UpdateLagCfgIntfList(ifName string, ifIndexList []int32) (bool, error)
-	GetBulkLag(curMark, count int) (*clntDefs.LagGetInfo, error)
+	GetBulkLag(curMark, count int) (*asicdClntDefs.LagGetInfo, error)
 
-	GetAllSubIPv4IntfState() ([]*clntDefs.SubIPv4IntfState, error)
+	GetAllSubIPv4IntfState() ([]*asicdClntDefs.SubIPv4IntfState, error)
+	GetSysRsvdVlan() int
+	GetIntfIdFromIfIndex(ifIndex int32) int
+	GetIntfTypeFromIfIndex(ifIndex int32) int
+	GetIfIndexFromIntfIdAndIntfType(ifId int, ifType int) int32
+	GetMinSysPort() int
 }
 
-func NewAsicdClntInit(clntPluginName AsicdClntPluginName, paramsFile string, asicdHdl clntDefs.AsicdClientStruct) (AsicdClntIntf, error) {
+func NewAsicdClntInit(clntPluginName AsicdClntPluginName, paramsFile string, asicdHdl asicdClntDefs.AsicdClientStruct) (AsicdClntIntf, error) {
 	switch clntPluginName {
 	case FS_ASICD_CLNT:
 		return fsAsicdClnt.NewAsicdClntInit(paramsFile, asicdHdl)
