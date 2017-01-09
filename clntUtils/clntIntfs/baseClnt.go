25f712336d4a2268fd1e9c58a75197ee9ed514bb
Harsha Kovuru <hkovuru@snaproute.com>
2017-01-09 13:01:21 -0800

    Add a argument to enable/disable thrift connection in the client utils.

--- clntUtils/clntIntfs/baseClnt.go
+++ clntUtils/clntIntfs/baseClnt.go
@@ -38,6 +38,7 @@ type NotifyMsg interface {
 type BaseClntInitParams struct {
 	Logger     logging.LoggerIntf
 	NHdl       NotificationHdl
+	EnableSHdl bool
 	ParamsFile string
 	PluginName string
 }
@@ -47,15 +48,19 @@ const (
 	FlexswitchClnt string = "Flexswitch"
 )
 
-func NewBaseClntInitParams(dmnName string, logger logging.LoggerIntf, nHdl NotificationHdl, paramsDir string) (*BaseClntInitParams, error) {
-	clntInfoFile := paramsDir + ClntInfoFile
-	pluginName, paramsFile, err := cfgParser.GetDmnClntInfoFromClntInfoJson(dmnName, clntInfoFile)
-	if err != nil {
-		return nil, err
+func NewBaseClntInitParams(dmnName string, logger logging.LoggerIntf, nHdl NotificationHdl, enableSHdl bool,
+	paramsDir string) (*BaseClntInitParams, error) {
+	if enableSHdl {
+		clntInfoFile := paramsDir + ClntInfoFile
+		pluginName, paramsFile, err := cfgParser.GetDmnClntInfoFromClntInfoJson(dmnName, clntInfoFile)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return &BaseClntInitParams{
 		Logger:     logger,
 		NHdl:       nHdl,
+		EnableSHdl: enableSHdl,
 		ParamsFile: paramsFile,
 		PluginName: pluginName,
 	}, nil
